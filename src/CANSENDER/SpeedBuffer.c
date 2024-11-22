84fb69000bce5162f8e68b1f6be2e5505381180b
AhmedAdelWafdy7 <fandes99@gmail.com>
2024-11-22 10:59:21 +0200

    add src to headunit

--- src/CANSENDER/SpeedBuffer.c
+++
@@ -1,7 +0,0 @@
-#include "SpeedBuffer.h"
-
-uint16_t speedBuffer[SPEED_BUFFER_SIZE] = {0};
-
-int speedBufferIndex = 0;
-
-pthread_mutex_t speedBufferMutex = PTHREAD_MUTEX_INITIALIZER;
--- src/HeadUnit/CarLoader.qml
+++
@@ -1,8 +0,0 @@
-import QtQuick 2.9
-
-Loader {
-    opacity: 1
-    asynchronous: true
-    active: true
-    source: "CarView.qml"
-}
